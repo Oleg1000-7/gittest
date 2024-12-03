f037718339385d28d7f27d1f48ce4dee7cefdd63
kukresh <oalzeg09@gmail.com>
2024-12-03 20:11:31 +0300

    second version

--- main.py
+++ main.py
@@ -1,74 +1,15 @@
 import sys
-import io
-
-from PyQt6 import uic
-from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
 from random import randint
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <layout class="QVBoxLayout" name="verticalLayout">
-    <item>
-     <spacer name="verticalSpacer">
-      <property name="orientation">
-       <enum>Qt::Vertical</enum>
-      </property>
-      <property name="sizeHint" stdset="0">
-       <size>
-        <width>20</width>
-        <height>40</height>
-       </size>
-      </property>
-     </spacer>
-    </item>
-    <item>
-     <widget class="QPushButton" name="btn">
-      <property name="text">
-       <string>кнопка</string>
-      </property>
-     </widget>
-    </item>
-   </layout>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>26</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from UI import Ui_MainWindow
 
 
-class MyWindow(QMainWindow):
+class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.do_paint = False
         self.w = self.width()
         self.h = self.height()
@@ -92,7 +33,7 @@ class MyWindow(QMainWindow):
 
     def draw(self, painter):
         painter: QPainter
-        painter.setBrush(QColor(255, 255, 0))
+        painter.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         r = randint(0, min(self.w, self.h))
         painter.drawEllipse(randint(0, self.w), randint(0, self.h), r, r)
 
